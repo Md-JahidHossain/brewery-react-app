20ed0777931e1a3d575912167b02088fd0905f5c
Md Jahid Hossain <raj_003144@yahoo.com>
2022-05-20 11:37:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='brewery/:id' element={<SingleBreweryDetails />} />
+        <Route path='brewery/:name' element={<SingleBreweryDetails />} />
       </Routes>
     </Router>
   );
